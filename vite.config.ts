11c55678f7571fd3abb0eedbcf2c6306d04552bb
Benjamin Mapesa <benleomapesa@gmail.com>
2026-04-07 02:54:06 +0300

    feat: add initial portfolio structure and remove base path from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  base: '/Ben-Mapesa-Portfolio/',
   plugins: [
     tailwindcss(),
-  ],
+  ]
 })
